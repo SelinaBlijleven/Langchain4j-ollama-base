f90bafbe6bbc0d900a792bf17a68095e5a55cbf7
Lina (Selina) Blijleven <12709907+SelinaBlijleven@users.noreply.github.com>
2026-01-09 13:53:27 +0100

    demo on structured output

--- src/main/java/codecafe/agenticllm/demos/OllamaDemoStructuredOutput.java
+++ src/main/java/codecafe/agenticllm/demos/OllamaDemoStructuredOutput.java
@@ -3,7 +3,6 @@
  * <p>
  * Previous: OllamaDemoRAG
  * <p>
- * @TODO: Demo on structured output
  * This is the last demo for the mini course, but the LangChain4J framework still offers
  * more to explore :)
  * <p>
@@ -14,9 +13,42 @@
  */
 package codecafe.agenticllm.demos;
 
+import codecafe.agenticllm.dto.Recipe;
+import codecafe.agenticllm.models.OllamaDemoModels;
+import codecafe.agenticllm.services.StructuredAssistant;
+import dev.langchain4j.service.AiServices;
+
 public class OllamaDemoStructuredOutput {
 
     public void demo() {
+        // LangChain will provide the output schema later.
+        String systemMsg = "You are Gordon Ramsay, a master recipe creator here to help users " +
+                "find the recipes for their favourite foods. You give every recipe a title, determine " +
+                "how many it serves and you include an ingredient list. You also include detailed " +
+                "steps for preparation.";
+        String prompt =
+                "Create a recipe for chocolate chip pancakes.";
+        System.out.println("System: " + systemMsg);
+        System.out.println("User: " + prompt);
+
+        // When we create structured output, we need to validate before outputting.
+        // This means that it's not compatible with streaming, so we will be
+        // building the AI service manually here. It might be a good idea to create
+        // your own builders or factories for this :)
+        StructuredAssistant assistant =
+                AiServices.builder(StructuredAssistant.class)
+                        .systemMessageProvider(_ -> systemMsg)
+                        .chatModel(OllamaDemoModels.defaultModel())
+                        .build();
+
+        // We can create a new recipe that follows the Recipe class perfectly.
+        // It seems like magic, but is the result of careful output validation and retries.
+        // Sometimes, the possible tokens are also restricted as a way to prevent invalid output
+        // when working with LLMs.
+        Recipe recipe = assistant.createRecipe(prompt);
+
+        System.out.println("Gordon Ramsay: ");
+        System.out.println(recipe);
     }
 
     public static void main(String[] args) {
